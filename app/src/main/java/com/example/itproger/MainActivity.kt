f8e043c3596195084e0148e495e74247ddf5fc61
gylymBatyr <gylymbatyr@gmail.com>
2024-11-24 18:26:38 +0500

    lesson 11

--- app/src/main/java/com/example/itproger/MainActivity.kt
+++ app/src/main/java/com/example/itproger/MainActivity.kt
@@ -6,8 +6,10 @@ import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.activity.ComponentActivity
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.compose.ui.graphics.Color
@@ -15,8 +17,14 @@ import androidx.core.content.ContextCompat
 
 
 class MainActivity : ComponentActivity() {
+    private val namesArr = arrayOf("John","Doe","Joe","Josh", "Jack", "George");
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.listView);
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.name_item, R.id.user_name, namesArr);
+        listView.adapter = adapter;
     }
 }
