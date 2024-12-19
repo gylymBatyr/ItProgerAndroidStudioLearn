034496db0b1bbc26c1ba726bbeb53eea70b05b7a
gylymBatyr <gylymbatyr@gmail.com>
2024-12-19 21:40:13 +0500

    lesson9

--- app/src/main/java/com/example/itproger/MainActivity.java
+++ app/src/main/java/com/example/itproger/MainActivity.java
@@ -12,42 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_fragment1, btn_fragment2;
-    private FrameLayout frameLayout;
-    private FirstFragment firstFragment = new FirstFragment();
-    private SecondFragment secondFragment = new SecondFragment();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_fragment1 = findViewById(R.id.btn_fragment1);
-        btn_fragment2 = findViewById(R.id.btn_fragment2);
-
-        setNewFragment(firstFragment);
-
-        btn_fragment1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setNewFragment(firstFragment);
-            }
-        });
-
-        btn_fragment2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setNewFragment(secondFragment);
-            }
-        });
     }
-
-    private void setNewFragment(Fragment fragment) {
-        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame_layout, fragment);
-        ft.addToBackStack(null);
-        ft.commit();
-    }
-
-
 }
