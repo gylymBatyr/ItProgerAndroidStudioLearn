493f81831482f19f1b9b819ea68fb16ae3060ec1
gylymBatyr <gylymbatyr@gmail.com>
2024-12-19 20:20:30 +0500

    lesson8

--- app/src/main/java/com/example/itproger/MainActivity.java
+++ app/src/main/java/com/example/itproger/MainActivity.java
@@ -3,56 +3,51 @@ package com.example.itproger;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton kakashi_image, naruto_image;
-    private MediaPlayer alert, windows_start;
+    private Button btn_fragment1, btn_fragment2;
+    private FrameLayout frameLayout;
+    private FirstFragment firstFragment = new FirstFragment();
+    private SecondFragment secondFragment = new SecondFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        kakashi_image = findViewById(R.id.kakashi_image);
-        naruto_image = findViewById(R.id.naruto_image);
+        btn_fragment1 = findViewById(R.id.btn_fragment1);
+        btn_fragment2 = findViewById(R.id.btn_fragment2);
 
-        alert = MediaPlayer.create(this, R.raw.red_alert);
-        windows_start = MediaPlayer.create(this, R.raw.windows_xp_start);
+        setNewFragment(firstFragment);
 
-        kakashi_image.setOnClickListener(new View.OnClickListener() {
+        btn_fragment1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                soundPlayButton(alert, windows_start);
+                setNewFragment(firstFragment);
             }
         });
 
-        naruto_image.setOnClickListener(new View.OnClickListener() {
+        btn_fragment2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                soundPlayButton(windows_start, alert);
+                setNewFragment(secondFragment);
             }
         });
-
     }
 
-    private void soundPlayButton(MediaPlayer sound1, MediaPlayer sound2){
-        if(sound1.isPlaying()){
-            sound1.pause();
-            sound1.seekTo(0);
-            sound1.setLooping(false);
-        }
-
-        if(sound2.isPlaying()){
-            sound2.pause();
-            sound2.seekTo(0);
-            sound2.setLooping(false);
-        }
-
-        sound1.start();
-        sound1.setLooping(true);
+    private void setNewFragment(Fragment fragment) {
+        FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_layout, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
     }
+
 
 }
